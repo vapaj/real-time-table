7d6f7da7c75d91e6dd2ffecc1f0aef00c7b43f77
Valtteri Pajunen <valtteri.pajunen@ilmedia.fi>
2016-06-12 09:29:16 +0300

    Add Käskynhaltijantie.

--- hsl_realtime.rb
+++ hsl_realtime.rb
@@ -4,11 +4,14 @@ require 'erb'
 require './lib/real_time_tables'
 
 get '/' do
-  timetable_kapyla  = RealTimeTables::Stop.new('kapyla')
-  timetable_maunula = RealTimeTables::Stop.new('maunula')
+  # TODO: Make stop ID an endpoint.
+  timetable_kapyla            = RealTimeTables::Stop.new(1250551)
+  timetable_maunula           = RealTimeTables::Stop.new(1282104)
+  timetable_kaskynhaltijantie = RealTimeTables::Stop.new(1283187)
+  @kapyla_to_helsinki            = construct_response(timetable_kapyla.get_next_20_departures)
+  @maunula_to_helsinki           = construct_response(timetable_maunula.get_next_20_departures)
+  @kaskynhaltijantie_to_helsinki = construct_response(timetable_kaskynhaltijantie.get_next_20_departures)
 
-  @kapyla_to_helsinki  = construct_response(timetable_kapyla.get_next_20_departures)
-  @maunula_to_helsinki =  construct_response(timetable_maunula.get_next_20_departures)
   erb :index
 end
 
--- lib/real_time_tables.rb
+++ lib/real_time_tables.rb
@@ -10,20 +10,15 @@ module RealTimeTables
 
   class Stop
     DEFAULT_COUNT = 20
-    STOPS = {
-      'kapyla'  =>  1250551,
-      'maunula' => 1282104,
-    }
 
-    def initialize(stop_name)
+    def initialize(stop_id)
       @hsl_api = SOAP::RPC::Driver.new(HSL_URL, INTERFACE_NAMESPACE)
       @hsl_api.add_method('getNextDeparturesExtRT', 'String_1', 'Date_2', 'int_3')
-      @stop_name = stop_name
-      raise "Stop named #{stop_name} not found" if !STOPS.key?(stop_name)
+      @stop_id = stop_id
     end
 
     def get_next_20_departures
-      @hsl_api.getNextDeparturesExtRT(STOPS[@stop_name], Time.now, DEFAULT_COUNT)
+      @hsl_api.getNextDeparturesExtRT(@stop_id, Time.now, DEFAULT_COUNT)
     end
 
     def parse_hsl_api_response(response)
